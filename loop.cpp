1d64c3f3dee95fe6ec771071577c77e69ecc0a71
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-11-05 02:13:35 -0800

    Add files via upload
    This is another example of for-loop in c++ with additional serial number. Now output will keep on printing and also its serial number along with each line will be printed

---
+++ loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+	{	int temp=1;
+	system("cls");
+	for(int x=1; x<21; x=x+1)
+{	
+		cout<<temp<<"): Welcome to UET Lahore"<<endl;
+		temp=temp+1;
+	}
+}
